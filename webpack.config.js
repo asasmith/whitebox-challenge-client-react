ee9507d3602d0b602c28d86d614504a30ba50cbc
Asa Smith <smith.asa.la@gmail.com>
2019-05-10 15:27:53 -0400

    add linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: './src/index.html',
   filename: './index.html',
-})
+});
 
 module.exports = {
   module: {
@@ -13,9 +13,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
+        },
       },
-    ]
+    ],
   },
   plugins: [htmlPlugin],
-}
+};
