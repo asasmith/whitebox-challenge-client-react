a7de366269588f9b4c712b4de222d55736aa11be
Asa Smith <smith.asa.la@gmail.com>
2019-05-12 15:23:32 -0400

    add hero component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import ProductsList from './components/products/ProductsList';
+import Hero from './components/hero/Hero';
+import ProductsLayout from './components/layouts/ProductsLayout';
 
 export default class App extends Component {
   render() {
     return (
       <div className="flex-sb flex-col min-h-view-max">
         <Header />
-        <ProductsList />
+        <Hero />
+        <ProductsLayout />
         <Footer />
       </div>
     );
