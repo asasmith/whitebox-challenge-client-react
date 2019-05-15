df5bd2658139720f2f3a7deb08529be1ea35ca47
Asa Smith <smith.asa.la@gmail.com>
2019-05-15 16:58:32 -0400

    more :expressionless:

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { DefinePlugin } = require('webpack');
 const merge = require('webpack-merge');
-const base = require('./webpack.config.js');
+const base = require('./webpack.base.js');
 
 const definePlugin = new DefinePlugin({
   _TEST_: JSON.stringify('production'),
